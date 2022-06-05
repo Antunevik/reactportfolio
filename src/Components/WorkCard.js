8343c7d8a7c17f4f41508ed63af7fdc4478f3595
Antuenvik <iantunevik@gmail.com>
2022-06-05 20:52:48 +0200

    added divider on workcard

--- src/Components/WorkCard.js
+++ src/Components/WorkCard.js
@@ -4,41 +4,50 @@ import { Divider, Typography, Grid, Stack, Button, Chip } from "@mui/material";
 const WorkCard = ({ image, title, chip, body, cta1, cta2 }) => {
   console.log({ image, title, chip, body, cta1, cta2 });
   return (
-    <Grid
-      container
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "row",
-      }}
-    >
-      <Grid item xs={12} md={6}>
-        <img src={image} alt={title} style={{ width: "100%" }} />
-      </Grid>
+    <>
       <Grid
-        item
-        p={2}
-        xs={12}
-        md={6}
+        container
+        py={5}
         sx={{
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          flexDirection: "column",
+          flexDirection: "row",
         }}
       >
-        <Typography>
-          {title} <Chip label={chip} />{" "}
-        </Typography>
-        <Typography>{body}</Typography>
-        <Stack direction={"row"} spacing={2}>
-          <Button>Preview</Button>
-          <Button>Github Repo</Button>
-        </Stack>
+        <Grid item xs={12} md={6}>
+          <img src={image} alt={title} style={{ width: "100%" }} />
+        </Grid>
+        <Grid
+          item
+          p={2}
+          xs={12}
+          md={6}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <Typography>
+            {title} <Chip label={chip} />{" "}
+          </Typography>
+          <Typography>{body}</Typography>
+          <Stack direction={"row"} spacing={2}>
+            <Button>Preview</Button>
+            <Button>Github Repo</Button>
+          </Stack>
+        </Grid>
       </Grid>
-      <Divider />
-    </Grid>
+
+      <Divider
+        variant="fullWidth"
+        sx={{ bgcolor: "darkgray" }}
+        xs={12}
+        md={6}
+      />
+    </>
   );
 };
 
